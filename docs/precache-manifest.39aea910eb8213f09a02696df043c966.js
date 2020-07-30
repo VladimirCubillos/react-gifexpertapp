b7ad8ff2cf6a1d90a1f42ea6ede6d4c844c0fb7f
Vladimir Cubillos <v.cubillosromn@gmail.com>
2020-07-30 18:39:39 -0400

    Docs agregado

---
+++ docs/precache-manifest.39aea910eb8213f09a02696df043c966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b5adec0109d79f94556497cbd7ed6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "758252a9bc64a359c731",
+    "url": "/static/css/main.4ae2e719.chunk.css"
+  },
+  {
+    "revision": "9948515d998a14a1480d",
+    "url": "/static/js/2.6a91807f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a91807f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "758252a9bc64a359c731",
+    "url": "/static/js/main.9b49d0d7.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
